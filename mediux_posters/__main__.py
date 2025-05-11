7021ccdabd5dfe6850bd3c5fb67ce59b437c3e31
BuriedInCode <6057651+Buried-In-Code@users.noreply.github.com>
2025-05-12 11:29:42 +1200

    Add main entry

--- mediux_posters/__main__.py
+++ mediux_posters/__main__.py
@@ -652,3 +652,6 @@ def set_posters(
                 media_type=media_type,
                 kometa_integration=settings.kometa_integration,
             )
+
+if __name__ == "__main__":
+    app(prog_name="Mediux-Posters")
